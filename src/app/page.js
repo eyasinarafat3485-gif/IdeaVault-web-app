6932e01011eb26b9c2424b69b04b7592b904697e
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-05-18 00:03:32 +0600

    HowItWorks modify

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
 import Banner from "@/Components/Banner";
 import PopularCategories from "@/Components/PopularCategories";
-import TrendingIdeas from "@/Components/TrendingIdeas";
-import Image from "next/image";
+import HowItWorks from "@/Components/HowItWorks";
 
 export default function Home() {
   return (
     <div >
       <Banner />
       <PopularCategories />
-      <TrendingIdeas />
+      <HowItWorks />
     </div>
   );
 }
